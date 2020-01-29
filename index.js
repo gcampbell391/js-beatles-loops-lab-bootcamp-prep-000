a76015efbcccb7f3ee6ff265771f2ae930561fe6
Gene Campbell III <gcampbell391@gmail.com>
2020-01-29 00:24:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// add solution here
+var musiciansArr = [];
+var instrumentsArr = [];
+var stringArr = [];
+function theBeatlesPlay(musiciansArr, instrumentsArr){
+  var musicString = '';
+  var counter = 0;
+  for(var i = 0; i < musiciansArr.length; i++){
+    musicString = `${musiciansArr[i]} plays ${instrumentsArr[i]}`;
+    stringArr.push(musicString);
+  }
+  return stringArr;
+}
+
+var factsArr = [];
+function johnLennonFacts(factsArr){
+ for(var i = 0; i <factsArr.length; i++){
+   factsArr[i] = `${factsArr[i]}!!!`
+ } 
+ return factsArr
+}
+var musiciansArr = [];
+var instrumentsArr = [];
+var stringArr = [];
+function theBeatlesPlay(musiciansArr, instrumentsArr){
+  var musicString = '';
+  var counter = 0;
+  for(var i = 0; i < musiciansArr.length; i++){
+    musicString = `${musiciansArr[i]} plays ${instrumentsArr[i]}`;
+    stringArr.push(musicString);
+  }
+  return stringArr;
+}
+
+var factsArr = [];
+function johnLennonFacts(factsArr){
+ for(var i = 0; i <factsArr.length; i++){
+   factsArr[i] = `${factsArr[i]}!!!`
+ } 
+ return factsArr
+}
+function iLoveTheBeatles(n){
+  var beatlesArr = [];
+  do{beatlesArr.push("I love the Beatles!");
+  }while(n < 15);
+  return beatlesArr;
+}
